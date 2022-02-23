962c7d2422cb7fa15d6526df852b65e26024741d
Archana <archanaputtur@gmail.com>
2022-02-23 14:47:05 +0530

    createdd project with create-react-app and added files to the home page

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import HomePage from "./homepage.component"; 
+
+function App() {
+  return (
+    <div className="App">
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
